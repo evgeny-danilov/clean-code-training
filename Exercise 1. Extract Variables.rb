b5986d3986c2818d32d63857194269035a3ba594
Evgenii Danilov <jmelkor@rabmler.ru>
2021-05-05 22:28:37 +0300

    Exercise #1: Extract Variables refactoring

--- Exercise 1. Extract Variables.rb
+++ Exercise 1. Extract Variables.rb
@@ -2,6 +2,8 @@
 
 class Tipper
 
+  TAX = 0.05
+
   def initialize(amount:, discount_percentage: 0, tip_percentage:)
     @amount = amount
     @discount_percentage = discount_percentage
@@ -9,7 +11,11 @@ class Tipper
   end
 
   def total
-    amount + (amount * 0.05) - (amount * (discount_percentage / 100.0)) + (amount * (tip_percentage / 100.0))
+    tax = amount * TAX
+    discount = amount * (discount_percentage / 100.0)
+    tip = amount * (tip_percentage / 100.0)
+    
+    amount + tax - discount + tip
   end
 
   private
