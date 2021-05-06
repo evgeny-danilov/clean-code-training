b5398f1356f5df2cbcbb408e54e951883a46add2
Evgenii Danilov <jmelkor@rabmler.ru>
2021-05-06 23:14:47 +0300

    Exercise #2: Extract Methdos

---
+++ Exercise 2. Extract Methods.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Tipper
+
+  TAX = 0.05
+
+  def initialize(amount:, discount_percentage: 0, tip_percentage:)
+    @amount = amount
+    @discount_percentage = discount_percentage
+    @tip_percentage = tip_percentage
+  end
+
+  def total
+    tax = amount * TAX
+    discount = amount * (discount_percentage / 100.0)
+    tip = amount * (tip_percentage / 100.0)
+    
+    amount + tax - discount + tip
+  end
+
+  private
+
+  attr_reader :amount, :discount_percentage, :tip_percentage
+
+end
