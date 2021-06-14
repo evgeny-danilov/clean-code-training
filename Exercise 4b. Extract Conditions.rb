d09c2dc501caa2275e18f92548f85857c767dbb1
Evgenii Danilov <jmelkor@rabmler.ru>
2021-06-14 20:17:27 +0300

    Exercise 4b: Extract Conditions refactoring

--- Exercise 4b. Extract Conditions.rb
+++ Exercise 4b. Extract Conditions.rb
@@ -4,8 +4,16 @@ class PhoneNumberFormatValidator < ActiveModel::Validator
   PHONE_REGEX = /^[0-9]{9}$/
 
   def validate(record)
-    unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+    unless valid_phone_number?(record)
       record.errors[:phone_number] << "invalid phone number format"
     end
+  end
+
+  private
+
+  def valid_phone_number?(record)
+    return true if record.phone_number.nil?
+
+    record.phone_number =~ PHONE_REGEX
   end
 end 
