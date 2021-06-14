39fcba701355a9a3b52fd1b9d4135575fbe81c4a
Evgenii Danilov <jmelkor@rabmler.ru>
2021-06-14 20:15:10 +0300

    Exercise 4a: DeMorgan Laws refactoring

--- Exercise 4a. DeMorgan Laws.rb
+++ Exercise 4a. DeMorgan Laws.rb
@@ -4,7 +4,7 @@ class PhoneNumberFormatValidator < ActiveModel::Validator
   PHONE_REGEX = /^[0-9]{9}$/
 
   def validate(record)
-    unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+    if record.phone_number.present? && !(record.phone_number =~ PHONE_REGEX)
       record.errors[:phone_number] << "invalid phone number format"
     end
   end
