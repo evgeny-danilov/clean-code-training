8ba5f38c9200f823363cb0da11dcaac2ad719976
Evgenii Danilov <jmelkor@rabmler.ru>
2021-06-14 20:19:19 +0300

    Exercise 4c: Remove If Statements refactoring

--- Exercise 4c. Remove If Statments.rb
+++ Exercise 4c. Remove If Statments.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 def benefit_rate
-  if (employee_on_vacation?)
-    if length_of_vacation > 1.week
-      return 0.5
-    end
-  end
+  return 0.5 if employee_on_vacation? && lenght_of_vacation > 1.week
 
   1.0
 end
