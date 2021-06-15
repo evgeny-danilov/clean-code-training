d9a1e7eb10d3f7f1f5a3082275cbdfb9c5e7f946
Evgenii Danilov <jmelkor@rabmler.ru>
2021-06-15 19:16:30 +0300

    Exercise 6b: Replace Conditionals With Strategies refactoring (v2)

--- Exercise 6b. Replace Conditional With Strategies.rb
+++ Exercise 6b. Replace Conditional With Strategies.rb
@@ -1,9 +1,36 @@
 # frozen_string_literal: true
 
+# That's important to have here just classes (not initialized instances), to avoid possible side-effects.
+GET_CITY_NAME_STRATEGIES = [
+  GetCityName::Minsk,
+  GetCityName::Berlin
+]
+
 def call
-  if address.start_with?('Minsk')
-    'Minsk'
-  elsif address.start_with?('Berlin')
-    'Berlin'
+  GET_CITY_NAME_STRATEGIES
+    .map(&:new)
+    .detect { _1.applicable?(address) }
+    .name
+end
+
+module GetCityName
+  class Minsk
+    def applicable?(address)
+      address.start_with?('Minsk')
+    end
+
+    def name
+      'Minsk'
+    end
+  end
+
+  class Berlin
+    def applicable?(address)
+      address.start_with?('Berlin')
+    end
+
+    def name
+      'Berlin'
+    end
   end
 end
