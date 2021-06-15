0d34efcb11f598d5c13e9cf0bc7ef4ba47f2c787
Evgenii Danilov <jmelkor@rabmler.ru>
2021-06-15 19:06:03 +0300

    Exercise #6a: Replace Conditonals With Constant Hash refactoring

--- Exercise 6a. Replace Conditional With Hash.rb
+++ Exercise 6a. Replace Conditional With Hash.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
+ACTIONS_BY_USER_TYPE = {
+  attendee: :register,
+  speaker:  :wake_up,
+  matz:     :make_tea  
+}
+
 def prepare_for_conference(user_type)
-  case user_type
-    when :attendee
-      :register
-    when :speaker
-      :wake_up
-    when :matz
-      :make_tea
-  end
+  ACTIONS_BY_USER_TYPE[user_type]
 end
+
+# They could be classes (strategies) as well:
+# 
+# ACTIONS_BY_USER_TYPE = {
+#   attendee: AttendeeStrategy,
+#   speaker:  SpeakerStrategy,
+#   matz:     MatzStrategy
+# }
+# ACTIONS_BY_USER_TYPE[user_type].new(Time.now).call
+# 
+# ...or even lambdas
+# 
+# ACTIONS_BY_USER_TYPE = {
+#   attendee: ->(time) { :register if ten_minutes_left?(time) },
+#   speaker:  ->(time) { :wake_up if one_hour_left?(time) },
+#   matz:     ->(_) { :make_tea }
+# }
+# ACTIONS_BY_USER_TYPE[user_type].call(Time.now)
