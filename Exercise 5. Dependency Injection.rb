d16c32b8f8c54498ad197592912d1c55b4472489
Evgenii Danilov <jmelkor@rabmler.ru>
2021-07-04 18:30:42 +0300

    Exersice 5: Dependency Injection refactoring

--- Exercise 5. Dependency Injection.rb
+++ Exercise 5. Dependency Injection.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class UserNotifier
-  def call(user, message, type)
-    if type == 'email'
-      EmailSender.send(user.email, message)
-    elsif type == 'sms'
-      SmsSender.send(user.email, message)
-    end
+  def call(user, message:, sender: EmailSender)
+    sender.send(user.email, message)
   end
 end
 
 notifier = UserNotifier.new
-notifier.call(user, "Hello World", 'email')
+notifier.call(user, message: "Hello World", sender: SmsSender)
