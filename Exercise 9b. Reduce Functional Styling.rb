88f78fc49279d7fa30447c468885286731c56cbc
Evgenii Danilov <jmelkor@rabmler.ru>
2021-09-13 19:45:49 +0300

    Exercise #9b: Reduce Functional Styling Refactoring

--- Exercise 9b. Reduce Functional Styling.rb
+++ Exercise 9b. Reduce Functional Styling.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 def call
-  GetValuationService.new(
-    Calculator.new(
-      CalculatorInput.new(params: params)
-    )
-  ).call
+  input_values = CalculatorInput.new(params: params)
+  calculator = Calculator.new(input_values)
+  
+  GetValuationService.new(calculator).call
 end
 
-# Some points regarding such an approach:
-#   1. It looks beautiful, but why?
-#   2. In fact, there is no way easy to debug it and check input params
-#   3. ...and no way to describe the code by meaningful naming
+# Now, you can place breakpoint in the method, and check all input variables
