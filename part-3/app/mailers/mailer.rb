cd2fb20fd9930b088e5b0fe2143506732a3e3ee9
Evgenii Danilov <jmelkor@rabmler.ru>
2021-10-16 15:53:35 +0300

    Fix Mailer naming

--- part-3/app/mailers/mailer.rb
+++ part-3/app/mailers/mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class AdminMailer < ApplicationMailer
+class Mailer < ApplicationMailer
   def invitation_notification(invitation, comment)
-    
+    # do something
   end
 end
