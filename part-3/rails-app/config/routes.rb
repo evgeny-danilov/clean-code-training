f88d762ef80be445085a5170e1ce6e60453fd61a
Evgenii Danilov <jmelkor@rabmler.ru>
2021-11-01 11:50:44 +0300

    Add logic in views

--- part-3/rails-app/config/routes.rb
+++ part-3/rails-app/config/routes.rb
@@ -2,4 +2,9 @@
 
 Rails.application.routes.draw do
   resources :invitations, only: %i[new create]
+  resources :reports, only: %i[show]
+
+  namespace :admin do
+    resources :reports, only: %i[show]
+  end
 end
