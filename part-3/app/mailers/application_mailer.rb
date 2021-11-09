b7c4860a022f07733edffe5e6592b46b5261405c
Evgenii Danilov <jmelkor@rabmler.ru>
2021-11-09 22:29:25 +0300

    Delete outdated Rails app

--- part-3/app/mailers/application_mailer.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationMailer < ActionMailer::Base
-  default from: 'test@mail.com'
-  layout 'mailer'
-end
--- part-3/app/mailers/mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Mailer < ApplicationMailer
-  def invitation_notification(invitation, comment)
-    # do something
-  end
-end
